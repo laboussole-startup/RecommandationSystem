3a8ecb18484b64018ae76d0fd38207b14296367a
laboussole-startup <laboussolesangmelima@gmail.com>
2024-06-23 01:10:54 +0100

    Votre message de commit ici

--- core/config.py
+++ core/config.py
@@ -4,26 +4,37 @@ from dotenv import load_dotenv
 from urllib.parse import quote_plus
 from pydantic_settings import BaseSettings
 
+# Définir le chemin du fichier .env
+env_path = Path('.') / '.env'
 
-env_path = Path(".") / ".env"
-
+# Charger les variables d'environnement depuis le fichier .env
 load_dotenv(dotenv_path=env_path)
+
+# Chemin du fichier CSV
 CSV_FILE_PATH = 'recommendation/formations_with_centres_interet.csv'
+
+
 class Settings(BaseSettings):
-        
-    # Database
-    DB_USER:str ="laboussole" #str = os.getenv('POSTGRES_USER')
-    DB_PASSWORD:str ="TnpAnsAhOOOXjDn7FphW6OYrKtcLk6qm" #str = os.getenv('POSTGRES_PASSWORD')
-    DB_NAME:str="laboussolebd" #str = os.getenv('POSTGRES_DB')
-    DB_HOST :str="dpg-co9abbq0si5c73994390-a.oregon-postgres.render.com" #str = os.getenv('POSTGRES_SERVER')
-    DB_PORT:str="5432" #str = os.getenv('POSTGRES_PORT')
-    DATABASE_URL: str = f"postgresql://{DB_USER}:{quote_plus(DB_PASSWORD)}@{quote_plus(DB_HOST)}:{quote_plus(DB_PORT)}/{quote_plus(DB_NAME)}"
-    # JWT 
+    # Database configuration
+    DB_USER: str = quote_plus(os.getenv('DB_USER'))
+    DB_PASSWORD: str = quote_plus(os.getenv('DB_PASSWORD'))
+    DB_NAME: str = quote_plus(os.getenv('DB_NAME'))
+    DB_HOST: str = quote_plus(os.getenv('DB_HOST'))
+    DB_PORT: str = quote_plus(os.getenv('DB_PORT'))
+    
+    # Construire l'URL de la base de données
+    DATABASE_URL: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    # JWT configuration
     JWT_SECRET: str = os.getenv('JWT_SECRET', '709d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7')
-    JWT_ALGORITHM: str = os.getenv('JWT_ALGORITHM', "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('JWT_TOKEN_EXPIRE_MINUTES', 60)
+    JWT_ALGORITHM: str = os.getenv('JWT_ALGORITHM', 'HS256')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv('JWT_TOKEN_EXPIRE_MINUTES', 60))
+
+
 def get_settings() -> Settings:
     return Settings()
 
 
-
+# Exemple d'utilisation
+settings = get_settings()
+print(settings.DATABASE_URL)  # Vérifiez que l'URL est correctement formée
